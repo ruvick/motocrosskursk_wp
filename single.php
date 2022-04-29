c814ce20bc04ebc29f27a9f2d2e9d8931ea8237f
Ruvick <rudikov.web@ya.ru>
2022-04-29 15:49:51 +0300

    Структура записи

--- single.php
+++ single.php
@@ -4,7 +4,26 @@
 
 <main class="page"> 
 
+<section id="banner-narrow" class="banner-narrow">
+	<div class="nuar_blk"></div>
+	<div class="_container">
+		<h1 class="banner-narrow__title"><? the_title();?></h1>
+	</div>
+</section>
 
+<section class="content page-recurring">
+  	<div class="_container">
+  		<?php
+				if ( function_exists('yoast_breadcrumb') ) {
+					yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );  
+				}
+			?> 
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<?php the_content();?>
+					<?php endwhile;?>
+				<?php endif; ?> 
+			</div>
+		</section>
 
 </main>
 

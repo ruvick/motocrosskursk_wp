213367b46838716bc6649583762eb0f68603b916
Ruvick <rudikov.web@ya.ru>
2022-04-29 12:58:42 +0300

    Структура основной страницы

--- page.php
+++ page.php
@@ -15,25 +15,28 @@
 
 get_header(); ?>
 
-<?php get_template_part('template-parts/header-section');?>
+<?php get_template_part('template-parts/header-section');?> 
 
 	<main id="primary" class="page site-main"> 
 
-		<section class="content"> 
-			<div class="_container">
+	<section id="banner-narrow" class="banner-narrow">
+		<div class="nuar_blk"></div>
+		<div class="_container">
+			<h1 class="banner-narrow__title"><? the_title();?></h1>
+		</div>
+	</section>
 
-			<?php
-			if ( function_exists('yoast_breadcrumb') ) {
-				yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
-			}
+  <section class="content page-recurring">
+  	<div class="_container">
+  		<?php
+				if ( function_exists('yoast_breadcrumb') ) {
+					yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );  
+				}
 			?> 
-
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<h1><?php the_title();?></h1>
 					<?php the_content();?>
 					<?php endwhile;?>
 				<?php endif; ?> 
-
 			</div>
 		</section>
 	</main>
